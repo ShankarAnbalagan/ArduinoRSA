6104cb89693ea7ae9fb835530781cca27724c501
Shankar Anbalagan <bravoshankar@gmail.com>
2018-09-29 09:02:24 +0530

    Second commit

---
+++ ArdRSA/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ArdRSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comm = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(108, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RSA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter port no.:";
+            // 
+            // comm
+            // 
+            this.comm.Location = new System.Drawing.Point(138, 165);
+            this.comm.Name = "comm";
+            this.comm.Size = new System.Drawing.Size(100, 20);
+            this.comm.TabIndex = 0;
+            this.comm.Text = "COM7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 288);
+            this.Controls.Add(this.comm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox comm;
+    }
+}
+
---
+++ ArdRSA/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace ArdRSA
+{
+    public partial class Form1 : Form
+    {
+        String[] indata = new String[7];
+        BigInteger cipher = new BigInteger();
+        BigInteger message = new BigInteger();
+        int n, e, d, data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String port=comm.Text;
+            serialPort1.PortName = port;
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("COM not available");
+            }
+        }
+
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            String data = serialPort1.ReadLine();
+            indata= data.Split(',');
+            if (indata.Length == 7)
+                convertData();
+        }
+
+        private void convertData()
+        {
+            
+            n = Convert.ToInt32(indata[2]);
+            e = Convert.ToInt32(indata[4]);
+            d = Convert.ToInt32(indata[5]);
+            data = Convert.ToInt32(indata[6]);
+            //MessageBox.Show("n ="+n+"\ne="+e+"\nd="+d+"\ndata="+data);
+            solve();
+        }
+
+        private void solve()
+        {
+            cipher = BigInteger.ModPow(data, e, n);
+            message = BigInteger.ModPow(cipher, d, n);
+            MessageBox.Show("n =" + n + "\ne=" + e + "\nd=" + d + "\ndata=" + data+"\nCipher= " +cipher+
+                "\nDecipher="+message);
+        }
+
+        ~Form1()
+        {
+            serialPort1.Close();
+        }
+    }    
+}
